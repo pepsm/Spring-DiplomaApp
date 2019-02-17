9d388b8979f9309021aa920e19973306eca72270
Peter Mihaylov <peps.mihailov@gmail.com>
2019-02-17 20:23:42 +0200

    add

--- src/main/java/springboot/JobsSpringbootApplication.java
+++ src/main/java/springboot/JobsSpringbootApplication.java
@@ -3,13 +3,15 @@ package springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"springboot"})
 @EntityScan(basePackageClasses = {
 		JobsSpringbootApplication.class,
 		Jsr310JpaConverters.class
@@ -22,6 +24,7 @@ public class JobsSpringbootApplication {
 	void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobsSpringbootApplication.class, args);
